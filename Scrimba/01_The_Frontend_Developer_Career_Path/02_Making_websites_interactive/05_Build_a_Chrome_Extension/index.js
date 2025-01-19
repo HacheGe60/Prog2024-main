7bb88b054e772ca3051176fa38e6a60418a9dfa3
HacheGe60 <horaciodev1706@gmail.com>
2025-01-19 20:36:34 -0300

    Imposible

--- Scrimba/01_The_Frontend_Developer_Career_Path/02_Making_websites_interactive/05_Build_a_Chrome_Extension/index.js
+++ Scrimba/01_The_Frontend_Developer_Career_Path/02_Making_websites_interactive/05_Build_a_Chrome_Extension/index.js
@@ -1,24 +1,21 @@
-let myLeads = [];
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import {
+    getDatabase,
+    ref, push
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+
+const firebaseConfig = {
+    databaseURL: process.env.DATABASE_URL
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const referenceInDB = ref(database, "leads");
+
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
-const tabBtn = document.getElementById('tab-btn');
-const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
-
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage;
-    render(myLeads);
-}
-
-tabBtn.addEventListener("click", function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        myLeads.push(tabs[0].url);
-        localStorage.setItem("myLeads", JSON.stringify(myLeads));
-        render(myLeads);
-    });
-});
+const deleteBtn = document.getElementById("delete-btn");
 
 function render(leads) {
     let listItems = "";
@@ -34,19 +31,12 @@ function render(leads) {
     ulEl.innerHTML = listItems;
 }
 
+deleteBtn.addEventListener("dblclick", function () {
+
+});
+
 inputBtn.addEventListener("click", function () {
-    myLeads.push(inputEl.value);
+    push(referenceInDB, inputEl.value);
+
     inputEl.value = "";
-    // Save the myLeads array to localStorage 
-    // PS: remember JSON.stringify()
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    render(myLeads);
 });
-
-deleteBtn.addEventListener('dblclick', function () {
-    localStorage.clear();
-    myLeads = [];
-    render(myLeads);
-});
-
-
