d055514d22066c8db36c340627a538e99f670ed5
lolodev <horaciodev1706@gmail.com>
2025-01-27 20:58:00 -0300

    Pumpkin's Purrfect Meme Picker Incomplete II

--- Scrimba/01_The_Frontend_Developer_Career_Path/05_Essential_JavaScript_Concepts/02_Pumpkins_Purrfect_Meme_Picker/index.js
+++ Scrimba/01_The_Frontend_Developer_Career_Path/05_Essential_JavaScript_Concepts/02_Pumpkins_Purrfect_Meme_Picker/index.js
@@ -1,34 +1,53 @@
-import { catsData } from "./data.js";
+import { catsData } from './data.js';
 
-const emotionRadios = document.querySelector("#emotion-radios");
+const emotionRadios = document.getElementById('emotion-radios');
+
+emotionRadios.addEventListener('change', highlightCheckedOption);
+
+function highlightCheckedOption(e) {
+    const radios = document.querySelectorAll('.radio');
+    for (let radio of radios) {
+        radio.classList.remove('highlight');
+    }
+
+    document.getElementById(e.target.id).parentElement.classList.add('highlight');
+}
+
 
 function getEmotionsArray(cats) {
     const emotionsArray = [];
-    for (const cat of cats) {
-        for (const emotion of cat.emotionTags) {
-            emotionsArray.push(emotion);
+    for (let cat of cats) {
+        for (let emotion of cat.emotionTags) {
+            if (!emotionsArray.includes(emotion)) {
+                emotionsArray.push(emotion);
+            }
         }
     }
     return emotionsArray;
 }
 
-function renderEmotionsRadios(cats) {
-    let radioItems = '';
-    const emotions = getEmotionsArray(cats);
 
-    for (const emotion of emotions) {
+function renderEmotionsRadios(cats) {
+
+    let radioItems = ``;
+    const emotions = getEmotionsArray(cats);
+    for (let emotion of emotions) {
         radioItems += `
         <div class="radio">
-        <label for="${emotion}">${emotion}</label>
-        <input 
-        type="radio" 
-        id="${emotion}" 
-        name="emotion" 
-        value="${emotion}">
-        </div>
-        `;
+            <label for="${emotion}">${emotion}</label>
+            <input
+            type="radio"
+            id="${emotion}"
+            value="${emotion}"
+            name="emotions"
+            >
+        </div>`;
     }
     emotionRadios.innerHTML = radioItems;
 }
 
-renderEmotionsRadios(catsData);
+renderEmotionsRadios(catsData)
+
+
+
+
