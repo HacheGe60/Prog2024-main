98204d04b57d83316fefd722e750c0079634caa8
lolodev <horaciodev1706@gmail.com>
2025-06-14 20:21:07 -0300

    Scoreboard

---
+++ Scrimba/01_The_Frontend_Developer_Career_Path/02_Making_websites_interactive/01_Build_a_passenger_counter_app/index.js
@@ -0,0 +1,16 @@
+// change the count-el in the HTML to reflect the new count
+
+let count = 0;
+
+function increment() {
+    count += 1;
+    //console.log(count);
+    document.getElementById("count-el").innerText = count;
+}
+
+22;
+18;
+65;
+"My Points: 59";
+4;
+1114;
