0523c2c7ca6c21d4f6861adbef8eedb4668a5895
lolodev <horaciodev1706@gmail.com>
2025-06-15 11:25:59 -0300

    Conmditional rules

---
+++ Scrimba/01_The_Frontend_Developer_Career_Path/02_Making_websites_interactive/03_Build a Blackjack_Game/index.js
@@ -0,0 +1,21 @@
+// 1. Create two variables, firstCard and secondCard. 
+// Set their values to a random number between 2-11
+let firstCard = 3;
+let secondCard = 10;
+
+// 2. Create a variable, sum, and set it to the sum of the two cards 
+let sum = firstCard + secondCard;
+
+// 3. Write the conditional according to these rules:
+
+// if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
+// else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
+// else -> "You're out of the game! 😭"
+
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂");
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳");
+} else {
+    console.log("You're out of the game! 😭");
+}
